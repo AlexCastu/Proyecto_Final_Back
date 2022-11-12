498167d6e9b6e70113852bf853b6d43e199eb7c9
AlexCastu <alexcastu@gmail.com>
2022-11-12 22:57:42 +0100

    intento de despliegue

--- services/getParapentes.js
+++ services/getParapentes.js
@@ -1,17 +1,21 @@
 const Parapentes = require("../models/ParapenteModel");
 
 const getParapentes = async (data) => {
+   console.log(data);
+   const precio = data.precio === "0" || data.precio === "undefined" ? 1000000 : data.precio;
+   let ordenPrecio = data.ordenPrecio === "1" ? { price: 1 } : data.ordenPrecio === "2" ? { price: -1 } : {};
+   let ordenMarca = data.ordenMarca === "1" ? { "marca.nombre": 1 } : data.ordenMarca === "2" ? { "marca.nombre": -1 } : {};
+   const filtroSort = data.ordenMarca === "3" ? ordenPrecio : ordenMarca;
 
-    const precio  =  data.precio  === "0" ||data.precio === "undefined" ? 1000000:data.precio;
-    let ordenPrecio = data.ordenPrecio === "1" ? {price:1}:data.ordenPrecio==="2"?{price:-1}:{};
-    let ordenMarca = data.ordenMarca === "1" ? {"marca.nombre":1}:data.ordenMarca==="2"?{"marca.nombre":-1}:{};
-    const filtroSort = data.ordenMarca === "3" ? ordenPrecio : ordenMarca;
-
-
-    return await Parapentes.paginate(
-           {"marca.nombre":data.fabricante||/.+/,categoria:data.categoria||/.+/, modelo: { $regex: data.busquedaManual, $options: "i" },price: { $lte:precio} },
-           { page:data.pagina , sort:filtroSort, limit: 10}
-      );
+   return await Parapentes.paginate(
+      {
+         "marca.nombre": data.fabricante || /.+/,
+         categoria: data.categoria || /.+/,
+         modelo: { $regex: data.busquedaManual, $options: "i" },
+         price: { $lte: precio },
+      },
+      { page: data.pagina, sort: filtroSort, limit: 10 }
+   );
 };
 
 module.exports = getParapentes;
